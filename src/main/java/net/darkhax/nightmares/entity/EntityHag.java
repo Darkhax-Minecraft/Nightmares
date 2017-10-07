64c5789d30dda6ed0e39a50b4d9731ba7b542197
Tyler Hancock <darklime@live.ca>
2017-10-07 13:47:37 -0600

    Fixed hag attributes.

--- src/main/java/net/darkhax/nightmares/entity/EntityHag.java
+++ src/main/java/net/darkhax/nightmares/entity/EntityHag.java
@@ -1,8 +1,8 @@
 package net.darkhax.nightmares.entity;
 
 import net.darkhax.bookshelf.entity.EntityPlayerMob;
+import net.darkhax.nightmares.entity.ai.EntityAINightmareAttackMelee;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
@@ -14,30 +14,30 @@ import net.minecraft.world.World;
 
 public class EntityHag extends EntityPlayerMob {
 
-	public EntityHag(World world) {
-		
-		super(world);		
-	}
-	
-	@Override
-    public void initEntityAI() {
-    	
+    public EntityHag (World world) {
+
+        super(world);
+    }
+
+    @Override
+    public void initEntityAI () {
+
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, new EntityAIAttackMelee(this, 1.0D, false));
+        this.tasks.addTask(2, new EntityAINightmareAttackMelee(this, 1.0D, false));
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
         this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
     }
-	
-	@Override
-    public void applyEntityAttributes() {
-		
+
+    @Override
+    public void applyEntityAttributes () {
+
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0D);
         this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
-        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.5D);
     }
 }
